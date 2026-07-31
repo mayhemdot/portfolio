17772cfe45972d338bacbbb168e9c8960285b52c
Evgenii Gor <p_mayhem@mail.ru>
2026-07-31 10:28:21 +0300

    chore(portfolio-site): change getServerSideURL

--- portfolio-site/src/utilities/getURL.ts
+++ portfolio-site/src/utilities/getURL.ts
@@ -1,13 +1,28 @@
 import canUseDOM from './canUseDOM'
 
+
 export const getServerSideURL = () => {
-  return (
-    process.env.NEXT_PUBLIC_SERVER_URL ||
-    (process.env.VERCEL_PROJECT_PRODUCTION_URL
-      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-      : 'http://localhost:3000')
-  )
-}
+  // Локальная переменная, если хочешь переопределить вручную
+  if (process.env.NEXT_PUBLIC_SERVER_URL) {
+    return process.env.NEXT_PUBLIC_SERVER_URL;
+  }
+
+  // Vercel Preview + Production
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  // Fallback
+  return "http://localhost:3000";
+};
+// export const getServerSideURL = () => {
+//   return (
+//     process.env.NEXT_PUBLIC_SERVER_URL ||
+//     (process.env.VERCEL_PROJECT_PRODUCTION_URL
+//       ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+//       : 'http://localhost:3000')
+//   )
+// }
 
 export const getClientSideURL = () => {
   if (canUseDOM) {
